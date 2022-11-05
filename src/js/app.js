9a76382ada777b36515d048c86f628fd01912abc
Filip Kwiatkowski <fi.kwiatkowski@gmail.com>
2022-11-05 19:14:23 +0100

    Ruchomy pojazd

--- src/js/app.js
+++ src/js/app.js
@@ -22,7 +22,7 @@ scene.background = new THREE.Color( 0x505050 );
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const orbit = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 5, 10);
+camera.position.set(0, 5, 15);
 camera.lookAt(0, 0, 0);
 orbit.update();
 
@@ -30,41 +30,50 @@ const hemisphereLight = new THREE.HemisphereLight(0x606060, 0x404040)
 scene.add(hemisphereLight);
 
 const light = new THREE.DirectionalLight(0xffffff, 0.8);
-light.position.set(4, 5, 3);
+light.position.set(10, 8, 8);
 scene.add(light);
 light.castShadow = true;
 
 light.shadow.mapSize.width = 2048;
 light.shadow.mapSize.height = 2048; 
 light.shadow.camera.near = 0.1; 
-light.shadow.camera.far = 12; 
+light.shadow.camera.far = 25; 
+light.shadow.camera.top = 10;
+light.shadow.camera.left = -10;
+light.shadow.camera.right = 25;
+light.shadow.camera.bottom = -10;
 
 const dLightHelper = new THREE.DirectionalLightHelper(light, 2);
 scene.add(dLightHelper);
 dLightHelper.visible = false;
+
+const dLightShadowHelper = new THREE.CameraHelper(light.shadow.camera);
+scene.add(dLightShadowHelper);
+dLightShadowHelper.visible = false;
 
 const axesHelper = new THREE.AxesHelper(10);
 scene.add(axesHelper);
 axesHelper.visible = false;
 
 const options = {
-    count: 20,
+    count: 00,
     velocity: {
         min: 1,
         max: 5
     },
     room: {
         object: null,
-        size: 6,
+        size: 15,
         segments: 10,
         visible: true,
-        cannonMaterial: new CANNON.Material()
+        cannonMaterial: new CANNON.Material(),
+        cannonBodies: [],
     },
     geometry: {
         object: null,
         segments: 3,
-        radiusMin: 0.08,
-        radiusMax: 0.2,
+        radiusMin: 0.2,
+        radiusMax: 0.5,
         wireframe: false,
         cannonMaterial: new CANNON.Material()
     },
@@ -75,13 +84,25 @@ const options = {
         object: null,
     },
     vehilce: {
-        cannonMaterial: new CANNON.Material()
+        cannonMaterial: new CANNON.Material(),
+        model: null,
+        cannon: null,
+        gltf: null,
+        wheelFrontOponaLeft: null,
+        wheelFrontOponaRight: null,
+        wheelFrontFelgaLeft: null, 
+        wheelFrontFelgaRight: null,
+        wheelBackOponaLeft: null,
+        wheelBackOponaRight: null,
+        wheelBackFelgaLeft: null,
+        wheelBackFelgaRight: null,
     },
     shadows: true,
     directLightIntensity: 0.8,
     hemisphereLightIntensity: 1,
     axesHelper: false,
     dLightHelper: false,
+    dLightShadowHelper: false,
     gravity: 9.8,
     orbitControl: true,
     dragControl: true,
@@ -134,33 +155,49 @@ const createRoom = () => {
     const groundBody = new CANNON.Body({ type: CANNON.Body.STATIC, shape: planeShape, position: new CANNON.Vec3(0, 0 - (.2 / 2),  0), material: options.room.cannonMaterial });
     groundBody.quaternion.setFromEuler(Math.PI / 2, 0, 0);
     world.addBody(groundBody);
+    options.room.cannonBodies.push(groundBody);
 
     const leftWallBody = new CANNON.Body({ type: CANNON.Body.STATIC, shape: planeShape, position: new CANNON.Vec3(-options.room.size / 2 - (.2 / 2), options.room.size / 2,  0), material: options.room.cannonMaterial });
     leftWallBody.quaternion.setFromEuler(0, Math.PI / 2, 0);
     world.addBody(leftWallBody);
+    options.room.cannonBodies.push(leftWallBody);
 
     const rightWallBody = new CANNON.Body({ type: CANNON.Body.STATIC, shape: planeShape, position: new CANNON.Vec3(options.room.size / 2 + (.2 / 2), options.room.size / 2,  0), material: options.room.cannonMaterial });
     rightWallBody.quaternion.setFromEuler(0, Math.PI / 2, 0);
     world.addBody(rightWallBody);
+    options.room.cannonBodies.push(rightWallBody);
 
     const backWallBody = new CANNON.Body({ type: CANNON.Body.STATIC, shape: planeShape, position: new CANNON.Vec3(0, options.room.size / 2,  -options.room.size / 2 - (.2 / 2)), material: options.room.cannonMaterial });
     world.addBody(backWallBody);
+    options.room.cannonBodies.push(backWallBody);
 
     const frontWallBody = new CANNON.Body({ type: CANNON.Body.STATIC, shape: planeShape, position: new CANNON.Vec3(0, options.room.size / 2,  options.room.size / 2 + (.2 / 2)), material: options.room.cannonMaterial });
     world.addBody(frontWallBody);
+    options.room.cannonBodies.push(frontWallBody);
 
     const topWallBody = new CANNON.Body({ type: CANNON.Body.STATIC, shape: planeShape, position: new CANNON.Vec3(0, options.room.size + (.2 / 2),  0), material: options.room.cannonMaterial });
     topWallBody.quaternion.setFromEuler(Math.PI / 2, 0, 0);
     world.addBody(topWallBody);
+    options.room.cannonBodies.push(topWallBody);
+}
 
-    /*
-    const boxGeometry = new THREE.BoxGeometry(options.room.size, options.room.size, .2);
-    const boxMaterial = new THREE.MeshBasicMaterial({color: '#fff000', side: THREE.DoubleSide});
-    const box = new THREE.Mesh(boxGeometry, boxMaterial);
-    scene.add(box);
-    box.position.copy(groundBody.position);
-    box.quaternion.copy(groundBody.quaternion);
-    */
+const removeVehicleBody = () => {
+    options.vehilce.cannon.removeFromWorld(world);
+}
+
+const removeVehicle = () =>{
+    removeVehicleBody();
+    scene.remove(options.vehilce.model);
+}
+
+const removeObjectBody = (object) => {
+    world.removeBody(object.userData.cannon);
+}
+
+const removeRoomBodies = () => {
+    while(options.room.cannonBodies.length){
+        world.removeBody(options.room.cannonBodies.pop());
+    }
 }
 
 const removeRoom = () => {
@@ -170,6 +207,9 @@ const removeRoom = () => {
 
 const resetRoom = () =>{
     removeRoom();
+    removeRoomBodies();
+    removeVehicle();
+    loadVehicleModel(options.vehilce.gltf);
     createRoom();
     addObjectsToRoom();
     controlObjects();
@@ -227,7 +267,9 @@ const addObjectsToRoom = () => {
 
 const removeObjectsFromRoom = () => {
     while(options.room.object.children.length){
-        options.room.object.remove(options.room.object.children[0]);
+        const object = options.room.object.children.pop();
+        removeObjectBody(object);
+        options.room.object.remove(object);
     }
 }
 
@@ -368,6 +410,10 @@ const createInterface = () => {
         dLightHelper.visible = e;
     });
 
+    helpers.add(options, 'dLightShadowHelper').onChange((e) => {
+        dLightShadowHelper.visible = e;
+    });
+
     const physics = options.gui.object.addFolder('Physics');
 
     physics.add(options, 'gravity').onChange((e) => {
@@ -384,15 +430,16 @@ const createInterface = () => {
     });
 }
 
-//const cannonDebugger = new CannonDebugger(scene, world, {});
+const cannonDebugger = new CannonDebugger(scene, world, {});
 const clock = new THREE.Clock()
 let delta
 
 function animate(tick){
     delta = Math.min(clock.getDelta(), 0.1)
-    world.fixedStep();
+    world.step(delta);
     moveObjects(delta);
-    //cannonDebugger.update();
+    moveVehicle(delta);
+    cannonDebugger.update();
     renderer.render(scene, camera);
 }
 
@@ -403,24 +450,220 @@ window.addEventListener('resize', () => {
 });
 
 const loadVehicleModel = (gltf) =>{
+    options.vehilce.gltf = gltf;
     const model = gltf.scene;
+    options.vehilce.model = model;
     scene.add(model);
-    model.position.set(0,(44.9 * 0.5));
-    model.getObjectByName('body_02_body_0_body_0_1').material.color.r = Math.random();
-    model.getObjectByName('body_02_body_0_body_0_1').material.color.g = Math.random();
-    model.getObjectByName('body_02_body_0_body_0_1').material.color.b = Math.random();
-    model.rotateY(Math.PI * 1);
 
-    model.traverse((node) => {
+    options.vehilce.model.getObjectByName('body_02_body_0_body_0_1').material.color.r = Math.random();
+    options.vehilce.model.getObjectByName('body_02_body_0_body_0_1').material.color.g = Math.random();
+    options.vehilce.model.getObjectByName('body_02_body_0_body_0_1').material.color.b = Math.random();
+
+    options.vehilce.wheelFrontOponaLeft = options.vehilce.model.getObjectByName('tire_mat5_04_tire_mat5_0');
+    options.vehilce.wheelFrontOponaRight = options.vehilce.model.getObjectByName('tire_mat5_03_tire_mat5_0');
+    options.vehilce.wheelFrontFelgaLeft = options.vehilce.model.getObjectByName('silver_11_silver_0'); 
+    options.vehilce.wheelFrontFelgaRight = options.vehilce.model.getObjectByName('silver_12_silver_0'); 
+
+    options.vehilce.wheelBackOponaLeft = options.vehilce.model.getObjectByName('tire_mat5_02_tire_mat5_0');
+    options.vehilce.wheelBackOponaRight = options.vehilce.model.getObjectByName('tire_mat5_01_tire_mat5_0');
+    options.vehilce.wheelBackFelgaLeft = options.vehilce.model.getObjectByName('silver_04_silver_0'); 
+    options.vehilce.wheelBackFelgaRight = options.vehilce.model.getObjectByName('silver_10_silver_0'); 
+
+    options.vehilce.model.traverse((node) => {
         if(node.isMesh){
             node.castShadow = true;
         }
     });
 
-    model.scale.set(.5, .5, .5);
-    const vehicleBB = new CANNON.Box(new CANNON.Vec3(0.5357977826044877,0.6064047883406146,1.2416590960797127));
-    const vehilceBody = new CANNON.Body({type: CANNON.Body.STATIC, shape: vehicleBB, material: options.vehilce.cannonMaterial});
-    world.addBody(vehilceBody);
+    options.vehilce.model.scale.set(.5, .5, .5);
+    const vehicleDimensions = new THREE.Box3().setFromObject(options.vehilce.model);
+    createVehicle(vehicleDimensions);
+}
+
+const steeringWheels = (deg) => {
+    options.vehilce.wheelFrontOponaLeft.rotation.z = 0;
+    options.vehilce.wheelFrontOponaRight.rotation.z = 0;
+    options.vehilce.wheelFrontFelgaLeft.rotation.z = 0;
+    options.vehilce.wheelFrontFelgaRight.rotation.z = 0;
+
+    options.vehilce.wheelFrontOponaLeft.rotation.y = -deg;
+    options.vehilce.wheelFrontOponaRight.rotation.y = -deg;
+    options.vehilce.wheelFrontFelgaLeft.rotation.y = -deg;
+    options.vehilce.wheelFrontFelgaRight.rotation.y = -deg;
+
+    options.vehilce.wheelFrontOponaLeft.rotation.x = 0;
+    options.vehilce.wheelFrontOponaRight.rotation.x = 0;
+    options.vehilce.wheelFrontFelgaLeft.rotation.x = 0;
+    options.vehilce.wheelFrontFelgaRight.rotation.x = 0;
+}
+
+const createVehicle = (vehicleDimensions) => {
+    const width = vehicleDimensions.max.x;
+    const height = vehicleDimensions.max.y / 2 - .1;
+    const deep = vehicleDimensions.max.z;
+
+    const carShape = new CANNON.Box(new CANNON.Vec3(width, height, deep));
+    const centerOfMassAdjust = new CANNON.Vec3(0, .2, 0);
+    
+    const carBody = new CANNON.Body({
+        mass: 5,
+        position: new CANNON.Vec3(0, 1, 0),
+    });
+
+    carBody.addShape(carShape, centerOfMassAdjust);
+
+    const vehicle = new CANNON.RigidVehicle({
+        chassisBody: carBody,
+    }); 
+
+    const wheelShape = new CANNON.Sphere(.2);
+    const wheelMaterial = new CANNON.Material('wheel');
+    const axis = new CANNON.Vec3(1,0,0);
+    const down = new CANNON.Vec3(0, -1, 0);
+
+    const leftFrontWheel = new CANNON.Body({
+        mass: 1, 
+        material: wheelMaterial,
+        angularDamping: .4,
+        shape: wheelShape,
+    });
+
+    const rightFrontWheel = new CANNON.Body({
+        mass: 1, 
+        material: wheelMaterial,
+        angularDamping: .4,
+        shape: wheelShape
+    });
+
+    const leftBackWheel = new CANNON.Body({
+        mass: 1, 
+        material: wheelMaterial,
+        angularDamping: .4,
+        shape: wheelShape
+    });
+
+    const rightBackWheel = new CANNON.Body({
+        mass: 1, 
+        material: wheelMaterial,
+        angularDamping: .4,
+        shape: wheelShape
+    });
+
+    vehicle.addWheel({
+        body: leftFrontWheel,
+        position: new CANNON.Vec3(width * .9, -height + .2, deep - .6),
+        axis: axis,
+        direction: down
+    });
+
+    vehicle.addWheel({
+        body: rightFrontWheel,
+        position: new CANNON.Vec3(-width * .9, -height + .2, deep - .6),
+        axis: axis,
+        direction: down
+    });
+    
+    vehicle.addWheel({
+        body: leftBackWheel,
+        position: new CANNON.Vec3(width * .9, -height + .2, -deep + .4),
+        axis: axis,
+        direction: down
+    });
+
+    vehicle.addWheel({
+        body: rightBackWheel,
+        position: new CANNON.Vec3(-width * .9, -height + .2, -deep + .4),
+        axis: axis,
+        direction: down
+    });
+
+    vehicle.addToWorld(world);
+    options.vehilce.cannon = vehicle;
+
+    document.addEventListener('keydown', (event) => {
+        const maxSteerVal = Math.PI / 8;
+        const maxForce = 5;
+
+        switch (event.key) {
+            case 'w':
+            case 'ArrowUp':
+            vehicle.setWheelForce(maxForce, 0);
+            vehicle.setWheelForce(maxForce, 1);
+            break;
+
+            case 's':
+            case 'ArrowDown':
+            vehicle.setWheelForce(-maxForce / 2, 0);
+            vehicle.setWheelForce(-maxForce / 2, 1);
+            break;
+
+            case 'a':
+            case 'ArrowLeft':
+            vehicle.setSteeringValue(maxSteerVal, 0);
+            vehicle.setSteeringValue(maxSteerVal, 1);
+            steeringWheels(maxSteerVal);
+            break;
+
+            case 'd':
+            case 'ArrowRight':
+            vehicle.setSteeringValue(-maxSteerVal, 0);
+            vehicle.setSteeringValue(-maxSteerVal, 1);
+            steeringWheels(-maxSteerVal);
+            break;
+        }
+    });
+  
+    // reset car force to zero when key is released
+    document.addEventListener('keyup', (event) => {
+        switch (event.key) {
+            case 'w':
+            case 'ArrowUp':
+            vehicle.setWheelForce(0, 0);
+            vehicle.setWheelForce(0, 1);
+            break;
+
+            case 's':
+            case 'ArrowDown':
+            vehicle.setWheelForce(0, 0);
+            vehicle.setWheelForce(0, 1);
+            break;
+
+            case 'a':
+            case 'ArrowLeft':
+            vehicle.setSteeringValue(0, 0);
+            vehicle.setSteeringValue(0, 1);
+            steeringWheels(0);
+            break;
+
+            case 'd':
+            case 'ArrowRight':
+            vehicle.setSteeringValue(0, 0);
+            vehicle.setSteeringValue(0, 1);
+            steeringWheels(0);
+            break;
+        }
+    });
+}
+
+let angularVelocity; 
+let velocity;
+
+const moveVehicle = (delta) => {
+    options.vehilce.model.position.copy(options.vehilce.cannon.chassisBody.position.clone().vadd(new CANNON.Vec3(0, -.2, 0)));
+    options.vehilce.model.quaternion.copy(options.vehilce.cannon.chassisBody.quaternion);
+
+    angularVelocity = options.vehilce.cannon.wheelBodies[0].angularVelocity.length();
+    velocity = options.vehilce.cannon.chassisBody.velocity;
+
+
+    options.vehilce.wheelFrontFelgaLeft.rotateX(angularVelocity * delta);
+    options.vehilce.wheelFrontFelgaRight.rotateX(angularVelocity * delta);
+    options.vehilce.wheelFrontOponaLeft.rotateX(angularVelocity * delta);
+    options.vehilce.wheelFrontOponaRight.rotateX(angularVelocity * delta);
+    options.vehilce.wheelBackFelgaLeft.rotateX(angularVelocity * delta);
+    options.vehilce.wheelBackFelgaRight.rotateX(angularVelocity * delta);
+    options.vehilce.wheelBackOponaLeft.rotateX(angularVelocity * delta);
+    options.vehilce.wheelBackOponaRight.rotateX(angularVelocity * delta);
 }
 
 const init = (gltf) => {
@@ -447,6 +690,7 @@ GLTFAssetLoader.load(VehicleModel.href, init, undefined, (error) => {
  * 2. Dodawanie kulek w wybranym miejscu za pomocą kursura
  * 3. Wstawić na środek jakiś ciekawy model (x)
  * 4. Odbijać kulki od modelu na środku (x)
- * 5. Prouszanie pojazdem
+ * 5. Prouszanie pojazdem (x)
+ * 6. Resetowanie pokoju ma restetować dodane ściany w cannon js (x)
  */
 
